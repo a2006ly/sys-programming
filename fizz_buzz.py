10d57cd26c84f2a6ec129b24681c0b45678b09bb
enbunsui <ywsrock@gmial.com>
2019-06-15 10:05:20 +0900

    lesson2fizz_buzz

---
+++ fizz_buzz.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def fb(n):
+    if n % 3 == 0 and n % 5 == 0:
+        return "FizzBuzz"
+    elif n % 3 == 0:
+        return "Fizz"
+    elif n % 5 == 0:
+        return "Buzz"
+    else:
+        return ""
+
+i=1
+while i <= 20:
+    print(i,fb(i))
+    i = i + 1
