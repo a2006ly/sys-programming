01d02ab0771d19cd5aa0b385f20ce1d59975747a
Administrator <172.20.10.73>
2019-06-27 17:31:37 +0900

    課題1 方法3追加

--- fizz_buzz02.py
+++ fizz_buzz02.py
@@ -7,13 +7,18 @@
     数字15で割れる時はFizzBuzz
 
 実現構想:
-    1.　ブール演算子の短絡評価特徴を利用して、　[and と or]を組み合わせて実現する。
+    1.　ブール演算子の短絡評価特徴を利用して、　[and と or]を組み合わせで実現する（1行で実現）。
            x or y　  x が偽なら y, そうでなければ x
    　　    x and y   x が偽なら x, そうでなければ y
     
-    2. 「辞書(key,value)　と　while」を利用して実現する。
+    2. 「辞書(key,value)　と　while」を利用して実現する（6行で実現）。
             除数と出力文字列をペア形で辞書に保存
             while の条件真と判定された場合、辞書から除数と対応の文字列取得
+    
+    3.  map関数、filter関数とlambdaを組み合わせで実現する（7行で実現）。
+            map関数でループ処理を行う
+            filter関数で条件チェックを行う
+            lambdaは計算処理を行う
 
 実現方法：
     Function Name :
@@ -61,6 +66,27 @@ def fb(n):
         while (n % key == 0):
              return dic[key]
     return ""
+
+i = 1
+while i <= 200:
+    print(i,fb(i))
+    i = i + 1
+
+
+
+'''
+実現方法3
+    mapとfilterを使て、条件を合った値を返す
+    以外の場合、空文字を返す
+'''
+
+def fb(n):
+    dic = {15:"FizzBuzz",5:"Buzz",3:"Fizz"}
+    try:
+       ll = list(filter(lambda x : n % x == 0, map(lambda k:k,dic.keys())))
+       return dic[ll[0]]
+    except :
+       return ""
 
 i = 1
 while i <= 200:
